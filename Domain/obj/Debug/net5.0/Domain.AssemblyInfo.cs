c4b16dd92bd21a1d53e6fb6a3c48f9b86553e1bb
Walter Alaniz <wallywally26@hotmail.es>
2024-07-29 16:52:09 -0300

    Agrego Home Docente

--- Domain/obj/Debug/net5.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net5.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c668083b4734d27c5b46d437498f530fb61b7fec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+340817e5ee5775c18661af9d91d8278fac0d2357")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
